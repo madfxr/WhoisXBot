2a48b253606008cde20b22a79fd8a52f4590da11
Nur Mukhammad Agus <30550561+madfxr@users.noreply.github.com>
2020-11-25 02:21:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
     if ($text == "/start") {
       file_put_contents($file);
-      $msg = "Welcome to WhoisXBot - WHOIS Lookup Utility: for TLD, gTLD, ccTLD and mTLD with Telegram Bot (https://github.com/xnxmx/whoisxbot)";
+      $msg = "Welcome to WhoisXBot - WHOIS Lookup Utility: for TLD, gTLD, ccTLD and mTLD with Telegram Bot (https://github.com/madfxr/whoisxbot)";
     }
 
     elseif ($text == "/whois") {
